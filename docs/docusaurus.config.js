ffe2c64b10f877d155da280b756b5a954c1da845
liubang <liubang01@baidu.com>
2022-01-20 19:17:47 +0800

    fix incorrect document link

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -32,7 +32,7 @@ module.exports = {
         },
         { to: 'blog', label: 'Blog', position: 'right' },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/algo-data-platform/LaserDB',
           label: 'GitHub',
           position: 'right',
         },
@@ -84,7 +84,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/algo-data-platform/LaserDB',
             },
           ],
         },
@@ -100,13 +100,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/algo-data-platform/LaserDB/edit/main/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/algo-data-platform/LaserDB/edit/main/docs/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
