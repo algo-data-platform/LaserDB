13948e398f31334cffc63b8c20928fa5215ae0c8
liubang <liubang@users.noreply.github.com>
2022-04-12 09:03:31 +0000

    deploy: ef49b6d52858294b8026080792da175255f0d38f

--- docs/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Volumes/workspace/github/liubang/LaserDB/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Volumes/workspace/github/liubang/LaserDB/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Volumes/workspace/github/liubang/LaserDB/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Volumes/workspace/github/liubang/LaserDB/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Volumes/workspace/github/liubang/LaserDB/docs/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Volumes/workspace/github/liubang/LaserDB/docs/src/css/custom.css"),
-];
